a9e8f6bbd08a386b1a94c940f62f3371b514cfbd
Giwan Persaud <giwan.persaud@gmail.com>
2015-10-05 23:15:12 +0200

    Adds material lite and uses it to create the header

--- src/component/main/main.js
+++ src/component/main/main.js
@@ -3,7 +3,32 @@ import React, {Component} from 'react';
 export default class Main extends Component {
   render () {
     return (
-      <h1>Hello World</h1>
+      <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+        <header className="mdl-layout__header">
+          <div className="mdl-layout__header-row">
+
+            <span className="mdl-layout-title">Title</span>
+
+            <div className="mdl-layout-spacer"></div>
+
+            <nav className="mdl-navigation mdl-layout--large-screen-only">
+              <a className="mdl-navigation__link" href="">Giwan Persaud <sup>100</sup></a>
+            </nav>
+          </div>
+        </header>
+        <div className="mdl-layout__drawer">
+          <span className="mdl-layout-title">Categories</span>
+          <nav className="mdl-navigation">
+            <a className="mdl-navigation__link" href="">World</a>
+            <a className="mdl-navigation__link" href="">Business</a>
+            <a className="mdl-navigation__link" href="">Sports</a>
+            <a className="mdl-navigation__link" href="">Science</a>
+          </nav>
+        </div>
+        <main className="mdl-layout__content">
+          <div className="page-content">content goes here </div>
+        </main>
+        </div>
     );
   }
 }
